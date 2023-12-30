47b4236e421a84b637441d31f9b1f3e085ed7dd2
Szabó Kristóf Tamás <szabokristof6@gmail.com>
2023-12-30 15:43:33 +0100

    fluent-ffmpeg, smaler segment sizes

--- routes/upload/uploadFile.js
+++ routes/upload/uploadFile.js
@@ -1,11 +1,9 @@
 const fs = require("fs");
-const { exec } = require("child_process");
+const ffmpeg = require("fluent-ffmpeg");
 
 require("dotenv").config();
 
 const File = require("../../models/File");
-
-const { geenrateUniqueId } = require("../utils");
 
 module.exports = async (req, res) => {
   if (!req.files || Object.keys(req.files).length === 0) {
@@ -20,14 +18,14 @@ module.exports = async (req, res) => {
     if (!uploadedFile.name.match(/\.(mp4|mov|avi)$/)) {
       return res.status(400).json({ error: "Please upload a valid video file." });
     }
-    
+
     // Create a new File object with uploaded file information
     const file = new File({
       name: uploadedFile.name,
       size: uploadedFile.size,
       owner: req.user._json.email,
     });
-    
+
     // Save the file information to MongoDB
     const savedFile = await file.save();
 
@@ -36,26 +34,28 @@ module.exports = async (req, res) => {
     // Create directory for HLS segments
     if (!fs.existsSync(outputDir)) {
       fs.mkdirSync(outputDir, { recursive: true });
-      
+
       fs.mkdirSync(`${outputDir}/segments`, { recursive: true });
     }
 
     // Use the mv() method to place the file somewhere on your server
     uploadedFile.mv(`${outputDir}/original`, function (err) {
       if (err) return res.status(500).send(err);
-      
-      // FFmpeg command for HLS conversion
-      const ffmpegCommand = `ffmpeg -i ${outputDir}/original -c:v copy -c:a copy -hls_time 10 -hls_list_size 0 -hls_segment_filename ${outputDir}/segments/segment_%03d.ts ${outputDir}/video.m3u8`;
-  
-      exec(ffmpegCommand, (error, stdout, stderr) => {
-        if (error) {
+
+      ffmpeg(`${outputDir}/original`)
+        .outputOptions(["-c:v copy", "-c:a copy", "-hls_time 2", "-hls_playlist_type vod", `-hls_segment_filename ${outputDir}/segments/segment_%03d.ts`])
+        .on("error", function (error) {
           console.error("Error converting video:", error);
           return res.status(500).json({ error: "Error converting video." });
-        }
-  
-        res.status(200).json({ message: `Success` });
-      });
+        })
+        .on("end", function () {
+          return res.status(200).json({ message: `Success` });
+        })
+        .on("progress", function (progress) {
+          console.log("Processing: " + progress.percent + "% done");
+        })
+        .output(`${outputDir}/video.m3u8`)
+        .run();
     });
-
   }
 };
